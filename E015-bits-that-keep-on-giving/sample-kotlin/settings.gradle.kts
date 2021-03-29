3db830d560d40455ab9222cd4eaa9455affcad0b
Pierre Goudjo <pierre@goudjo.xyz>
2021-03-29 05:50:57 +0200

    Remove persistence module

--- E015-bits-that-keep-on-giving/sample-kotlin/settings.gradle.kts
+++ E015-bits-that-keep-on-giving/sample-kotlin/settings.gradle.kts
@@ -1,7 +1,6 @@
 rootProject.name = "E015"
 include("domain")
 include("console")
-include("persistence")
 include("ddd-cqrs-event-sourcing")
 include("ktcodec-kotlinx-serialization-json")
 include("ktcodec")
